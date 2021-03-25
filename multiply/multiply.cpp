b03f09abc51d4ced4bbfa894a5e4601b69e42492
MariaIvanova01 <mimi200801@abv.bg>
2021-03-25 17:35:38 +0200

    Logic for addition of two integers implemented

---
+++ multiply/multiply.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double n, m, d;
+	cout << "n = ";
+	cin >> n;
+	cout << "m = ";
+	cin >> m;
+	d = n * m;
+	cout << d << endl;
+
+
+}
+
